e68c437e4e646cdb51d34f174977c3aff40c2cd8
Omar Emad <oemad3366@gmail.com>
2026-03-21 21:01:01 +0000

    Login partial has been created

--- ViewModel/VerifyEmailViewModel.cs
+++ ViewModel/VerifyEmailViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace UserApp.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApp.ViewModels
 {
     public class VerifyEmailViewModel
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
